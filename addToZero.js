9298061f1f3b6c70a62f5251d0275fc47eef995e
GulenYilmaz <gulenaltintasyilmaz@gmail.com>
2021-10-18 09:33:05 -0400

    challenge-add-to-zero is done

---
+++ addToZero.js
@@ -0,0 +1,22 @@
+// Starting array
+let array = [28, 43, -12, 30, 4, 0, 12]
+
+// Write your solution below:
+console.log(true+"12+(-12)=0")
+
+
+// Starting array
+let arr = [0, 21, 33, 6, 0, -9]
+
+
+// Write your solution below:
+console.log(true+"0+0=0")
+
+
+
+// Starting array
+let a = [0, 1, 2, 3, 4, 5]
+
+
+// Write your solution below:
+console.log(false+"there is no two numbers in the array add to zero")
